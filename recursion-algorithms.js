f703b073a6bfe25eddc640e82db9061718e2c75e
Michael Dusaniwskyj <dusaniwskyj@gmail.com>
2019-07-22 16:52:34 -0400

    finished question 8 and 9

--- recursion-algorithms.js
+++ recursion-algorithms.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 'use strict';
 
 function countSheep(sheep) {
@@ -57,13 +58,58 @@ function stringSplitter(str, seperator = '') {
 // console.log(stringSplitter('02/20/2020', '/'));
 
 function fibonacci(n) {
-  if (n === 0) {
-    return 0;
-  }
-  if (n === 1) {
-    return 1;
-  }
+  if (n === 0) return 0;
+  if (n === 1) return 1;
   return fibonacci(n - 1) + fibonacci(n - 2);
 }
 
-console.log(fibonacci(6));
+// console.log(fibonacci(6));
+
+
+function factorial(n) {
+  if (n === 0) return 1;
+  return (factorial(n-1) * n );
+}
+
+// console.log(factorial(10));
+
+
+let mySmallMaze = [
+  [' ', ' ', ' '],  
+  [' ', '*', ' '],
+  [' ', ' ', 'e']
+];
+
+let maze = [
+  [' ', ' ', ' ', '*', ' ', ' ', ' '],
+  ['*', '*', ' ', '*', ' ', '*', ' '],
+  [' ', ' ', ' ', ' ', ' ', ' ', ' '],
+  [' ', '*', '*', '*', '*', '*', ' '],
+  [' ', ' ', ' ', ' ', ' ', ' ', 'e']
+];
+
+function findWayOut(maze, position = 0, row, column, direction = 'S', path) {
+  if ( column < 0 || row < 0 ) return; 
+  if ( column >= maze[0].length || row >= maze.length ) return;
+
+  path[position] = direction;
+  position++;
+
+  if ( maze[row][column] === 'e') {
+    console.log( 'found path :', path);
+    return;
+  }
+
+  if (maze[row][column] === ' ') {
+    maze[row][column] = 'x';
+    findWayOut(maze, position, row, column -1, 'L', path);
+    findWayOut(maze, position, row -1, column, 'U', path);
+    findWayOut(maze, position, row, column +1, 'R', path);
+    findWayOut(maze, position, row +1, column, 'D', path);
+    //maze[row][column] = ' ';
+  }
+  position--;
+}
+
+findWayOut(maze, 0 , 0 , 0 , 'S' , [] );
+
